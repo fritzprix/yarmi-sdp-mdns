5cf9cdd30543ebf4edcd234512d455b4b7e3fee2
fritzprix <innocentevil0914@gmail.com>
2019-06-23 23:07:44 +0900

    remove all files

--- src/test/java/TestController.java
+++
@@ -1,9 +0,0 @@
-import net.doodream.yarmi.annotation.method.RMIExpose;
-import net.doodream.yarmi.annotation.parameter.Body;
-import net.doodream.yarmi.model.Response;
-
-public interface TestController {
-
-    @RMIExpose
-    Response<String> hello(@Body final String message);
-}
